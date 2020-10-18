df590a1dd22a10a0e93d71edca79cf403122b233
Tomi <tomi.mavrin@gmail.com>
2020-10-18 19:20:03 +0200

    tutorial files

---
+++ README.md
@@ -0,0 +1,10 @@
+### Job Ninja
+Code from following a tutorial on Sapper.
+
+#### Tutorial
+[Sapper Tutorial (Crash Course)](https://www.youtube.com/playlist?list=PL4cUxeGkcC9gdr4Qhx83gBBcID-KMe-PQ)
+
+##### Total Lenght Of Tutorial:
+
+1 hour and 45 minutes
+
---
+++ src/routes/jobs/_data.js
@@ -0,0 +1,7 @@
+let paragraph = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, optio. Repudiandae dolorem doloremque necessitatibus distinctio natus debitis excepturi quaerat ullam quis, sapiente nesciunt, temporibus nulla, id ipsum assumenda maxime perspiciatis!';
+
+export let jobs = [
+    { id: '1', title: "Mario's plumming assistant", salary: 20000, details: paragraph },
+    { id: '2', title: "Apple picker for Yoshi", salary: 25000, details: paragraph },
+    { id: '3', title: "Mariokart driver", salary: 30000, details: paragraph }
+];
